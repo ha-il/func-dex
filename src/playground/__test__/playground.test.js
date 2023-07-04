30d9e5661c77fb82fd86fcf12ac66de5ff93bc7a
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-07-04 17:52:19 +0900

    chore: Set up jest

---
+++ src/playground/__test__/playground.test.js
@@ -0,0 +1,8 @@
+import playground from "../playground";
+
+describe("playground", () => {
+  it("플레이그라운드 테스트", () => {
+    // Replace the next dummy test with your playground function tests.
+    expect(playground()).toBe(4);
+  });
+});
---
+++ src/playground/playground.js
@@ -0,0 +1,7 @@
+import { add } from "../functions/operation/operation.js";
+
+function playground() {
+  return add(2, 2);
+}
+
+export default playground;
