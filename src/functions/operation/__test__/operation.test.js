8debefe156f120fc453c1253c416d09a0587ff79
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-07-04 17:59:07 +0900

    fix: Fix typo of test description in opertation

--- src/functions/operation/__test__/operation.test.js
+++ src/functions/operation/__test__/operation.test.js
@@ -3,4 +3,4 @@ import { add, subtract, multiply, divide } from "../operation.js";
 test("2 + 2 = 4", () => expect(add(2, 2)).toBe(4));
 test("2 - 2 = 0", () => expect(subtract(2, 2)).toBe(0));
 test("2 * 2 = 4", () => expect(multiply(2, 2)).toBe(4));
-test("2 / 2 = 4", () => expect(divide(2, 2)).toBe(1));
+test("2 / 2 = 1", () => expect(divide(2, 2)).toBe(1));
