29593caf87410101cb4dd3185e48aec3224f3b25
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-07-10 16:47:10 +0900

    Feat: Update functions of functional programming

---
+++ src/functions/fp/FP.js
@@ -0,0 +1,174 @@
+// FP.js의 파일의 함수는 유인동 개발자님의 functional-javascript-01 저장소를 참고했음을 밝힙니다.
+// 저장소 링크: https://github.com/indongyoo/functional-javascript-01
+// 관련 강의: 함수형 프로그래밍과 JavaScript ES6+
+// 강의 링크: https://www.inflearn.com/course/functional-es6
+
+// curry
+export const curry =
+  (f) =>
+  (a, ..._) =>
+    _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+// isIterable
+export const isIterable = (a) => {
+  return a != null && !!a[Symbol.iterator];
+};
+
+// Promise
+// go1, nop, reduceF, head, delay
+export const go1 = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
+
+export const nop = Symbol("nop");
+
+export const reduceF = (acc, a, f) =>
+  a instanceof Promise
+    ? a.then(
+        (a) => f(acc, a),
+        (e) => (e == nop ? acc : Promise.reject(e))
+      )
+    : f(acc, a);
+export const head = (iter) => go1(take(1, iter), ([h]) => h);
+
+export const delay = curry((time, a) => {
+  new Promise((resolve) => setTimeout(resolve, time));
+  return a;
+});
+
+// Lazy
+// L.map, L.filter, L.range
+export const L = {
+  map: curry(function* (f, iter) {
+    for (const a of iter) {
+      yield go1(a, f);
+    }
+  }),
+  filter: curry(function* (f, iter) {
+    for (const a of iter) {
+      const b = go1(a, f);
+      if (b instanceof Promise)
+        yield b.then((b) => (b ? a : Promise.reject(nop)));
+      else if (b) yield a;
+    }
+  }),
+  range: function* (l) {
+    let i = -1;
+    while (++i < l) {
+      yield i;
+    }
+  },
+};
+
+// reduce, take
+export const reduce = curry((f, acc, iter) => {
+  if (!iter) return reduce(f, head((iter = acc[Symbol.iterator]())), iter);
+
+  iter = iter[Symbol.iterator]();
+  return go1(acc, function recur(acc) {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      acc = reduceF(acc, cur.value, f);
+      if (acc instanceof Promise) return acc.then(recur);
+    }
+    return acc;
+  });
+});
+
+export const take = curry((l, iter) => {
+  let res = [];
+  iter = iter[Symbol.iterator]();
+  return (function recur() {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      if (a instanceof Promise) {
+        return a
+          .then((a) => ((res.push(a), res).length == l ? res : recur()))
+          .catch((e) => (e == nop ? recur() : Promise.reject(e)));
+      }
+      res.push(a);
+      if (res.length == l) return res;
+    }
+    return res;
+  })();
+});
+
+// go, pipe, takeAll
+export const go = (...args) => reduce((a, f) => f(a), args);
+
+export const pipe =
+  (f, ...fs) =>
+  (...as) =>
+    go(f(...as), ...fs);
+
+export const takeAll = take(Infinity);
+
+// map, filter, range
+export const map = curry(pipe(L.map, takeAll));
+
+export const filter = curry(curry(pipe(L.filter, takeAll)));
+
+export const range = (l) => {
+  let i = -1;
+  let res = [];
+  while (++i < l) {
+    res.push(i);
+  }
+  return res;
+};
+
+// Flat
+// L.flatten, L.deepFlat, L.flatMap
+L.flatten = function* (iter) {
+  for (const a of iter) {
+    if (isIterable(a)) yield* a;
+    else yield a;
+  }
+};
+
+L.deepFlat = function* f(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) yield* f(a);
+    else yield a;
+  }
+};
+
+L.flatMap = curry(pipe(L.map, L.flatten));
+
+// flatten, deepFlat, flatMap
+export const flatten = pipe(L.flatten, takeAll);
+
+export const deepFlat = pipe(L.deepFlat, takeAll);
+
+export const flatMap = (f, iter) => L.flat(L.map(f, iter));
+
+// Utils
+// join, find
+export const join = curry((sep = ",", iter) =>
+  reduce((a, b) => `${a}${sep}${b}`, iter)
+);
+
+export const find = curry((f, iter) =>
+  go(iter, L.filter(f), take(1), ([v]) => v)
+);
+
+// Concurrency
+// noop, catchNoop
+const noop = () => {};
+
+const catchNoop = (arr) => (
+  arr.forEach((a) => (a instanceof Promise ? a.catch(noop) : a)), arr
+);
+
+// C.reduce, C.take
+export const C = {
+  reduce: curry((f, acc, iter) => {
+    const iter2 = catchNoop(iter ? [...iter] : [...acc]);
+    return iter ? reduce(f, acc, iter2) : reduce(f, iter2);
+  }),
+  take: curry((l, iter) => take(l, catchNoop([...iter]))),
+};
+
+// C.map, C.filter
+C.takeAll = C.take(Infinity);
+C.map = curry(pipe(L.map, C.takeAll));
+C.filter = curry(pipe(L.filter, C.takeAll));
---
+++ src/functions/fp/__test__/FP.test.js
@@ -0,0 +1,219 @@
+import {
+  map,
+  filter,
+  reduce,
+  go,
+  pipe,
+  range,
+  L,
+  take,
+  join,
+  find,
+  flatten,
+  deepFlat,
+  go1,
+  delay,
+  C,
+} from "../FP.js";
+
+const f = pipe(
+  (a) => a + 1,
+  (a) => a + 10,
+  (a) => a + 100
+);
+
+const g = pipe(
+  (a, b) => a + b,
+  (a) => a + 10,
+  (a) => a + 100
+);
+
+const data = {
+  keypad: {
+    id: 0,
+    grid: [
+      [
+        '<svg data-testid="1"><g></svg>',
+        '<svg data-testid="2"><g></svg>',
+        '<svg data-testid="9"><g></svg>',
+        '<svg data-testid="blank1"><g></svg>',
+      ],
+      [
+        '<svg data-testid="4"><g></svg>',
+        '<svg data-testid="0"><g></svg>',
+        '<svg data-testid="5"><g></svg>',
+        '<svg data-testid="3"><g></svg>',
+      ],
+    ],
+  },
+};
+
+test("map", () => expect(map((v) => v + 1, [1, 2, 3])).toEqual([2, 3, 4]));
+
+test("filter", () =>
+  expect(filter((p) => p % 2, [1, 2, 3, 4, 5])).toEqual([1, 3, 5]));
+
+test("reduce", () => expect(reduce((a, b) => a + b, 0, [1, 2, 3])).toBe(6));
+
+test("reduce: no acc", () =>
+  expect(reduce((a, b) => a + b, [1, 2, 3])).toBe(6));
+
+test("go", () =>
+  expect(
+    go(
+      0,
+      (a) => a + 1,
+      (a) => a + 10,
+      (a) => a + 100
+    )
+  ).toBe(111));
+
+test("pipe: 1 arg", () => expect(f(0)).toBe(111));
+
+test("pipe: 2 args", () => expect(g(0, 1)).toBe(111));
+
+test("go, map, filter, reduce", () =>
+  expect(
+    go(
+      [1, 2, 3, 4, 5],
+      (iter) => map((v) => v * v, iter),
+      (iter) => filter((v) => v % 2, iter),
+      (iter) => reduce((a, b) => a + b, iter)
+    )
+  ).toBe(35));
+
+test("curry: go, map, filter, reduce", () =>
+  expect(
+    go(
+      [1, 2, 3, 4, 5],
+      map((v) => v * v),
+      filter((v) => v % 2),
+      reduce((a, b) => a + b)
+    )
+  ).toBe(35));
+
+test("range", () => expect(range(5)).toEqual([0, 1, 2, 3, 4]));
+
+test("L.range", () => expect(reduce((a, b) => a + b, L.range(5))).toBe(10));
+
+test("take", () => expect(go(range(100), take(3))).toEqual([0, 1, 2]));
+
+test("L.map", () =>
+  expect([...L.map((v) => v * v, [1, 2, 3])]).toEqual([1, 4, 9]));
+
+test("L.filter", () =>
+  expect([...L.filter((v) => v % 2, [1, 2, 3])]).toEqual([1, 3]));
+
+test("join", () => expect(join("/", [2023, 7, 8])).toBe("2023/7/8"));
+
+test("find", () =>
+  expect(find((v) => v === "c", ["a", "b", "c", "d"])).toBe("c"));
+
+test("L.flatten", () =>
+  expect([...L.flatten([[1, 2], [3, 4], 5])]).toEqual([1, 2, 3, 4, 5]));
+
+test("flatten", () =>
+  expect(flatten([[1, 2], [3, 4], 5])).toEqual([1, 2, 3, 4, 5]));
+
+test("L.deepFlat", () =>
+  expect([...L.deepFlat([[[[[1], 2], 3], 4], 5])]).toEqual([1, 2, 3, 4, 5]));
+
+test("deepFlat", () =>
+  expect(deepFlat([[[[[1], 2], 3], 4], 5])).toEqual([1, 2, 3, 4, 5]));
+
+test("2d array", () =>
+  expect(
+    go(
+      data.keypad.grid,
+      flatten,
+      map((s) => s.match(/data-testid="([^"]+)"/)[1])
+    )
+  ).toEqual(["1", "2", "9", "blank1", "4", "0", "5", "3"]));
+
+test("delay go1", () => expect(go1(delay(100, 10), (a) => a + 5)).toBe(15));
+
+test("go with Promise", async () =>
+  expect(
+    await go(
+      1,
+      (a) => a + 10,
+      (a) => Promise.resolve(a + 100),
+      (a) => a + 1000
+    )
+  ).toBe(1111));
+
+test("Lazy Promise", async () => {
+  expect(
+    await go(
+      [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)],
+      L.map((a) => a + 10),
+      take(2)
+    )
+  ).toEqual([11, 12]);
+});
+
+test("Lazy not Promise", async () => {
+  expect(
+    await go(
+      [1, 2, 3],
+      L.map((a) => a + 10),
+      take(2)
+    )
+  ).toEqual([11, 12]);
+});
+
+test("filter nop", async () => {
+  expect(
+    await go(
+      [1, 2, 3, 4, 5, 6],
+      L.map((a) => Promise.resolve(a * a)),
+      L.filter((a) => a % 2),
+      take(2)
+    )
+  ).toEqual([1, 9]);
+});
+
+test("reduce nop", async () => {
+  expect(
+    await go(
+      [1, 2, 3, 4],
+      L.map((a) => Promise.resolve(a * a)),
+      L.filter((a) => Promise.resolve(a % 2)),
+      reduce((a, b) => a + b)
+    )
+  ).toBe(10);
+});
+
+test("C.reduce", async () => {
+  expect(
+    await go(
+      [1, 2, 3, 4, 5],
+      L.map((a) => delay(1000, a * a)),
+      L.filter((a) => delay(1000, a % 2)),
+      C.reduce((a, b) => a + b)
+    )
+  ).toBe(35);
+});
+
+test("C.take", async () => {
+  expect(
+    await go(
+      [1, 2, 3, 4, 5],
+      L.map((a) => delay(1000, a * a)),
+      L.filter((a) => delay(1000, a % 2)),
+      C.take(2)
+    )
+  ).toEqual([1, 9]);
+});
+
+test("C.map", async () => {
+  expect(await C.map((a) => delay(1000, a * a), [1, 2, 3, 4])).toEqual([
+    1, 4, 9, 16,
+  ]);
+});
+
+test("C.filter", async () => {
+  expect(await C.filter((a) => delay(1000, a % 2), [1, 2, 3, 4])).toEqual([
+    1, 3,
+  ]);
+});
