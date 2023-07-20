08ce4f27633f99e0c3f2e3b4833c21b3d905a5ee
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-07-20 13:21:52 +0900

    feat: Add insert method to BinarySearchTree class

---
+++ src/functions/data-structures/binary-search-tree/BinarySearchTree.js
@@ -0,0 +1,37 @@
+export class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (value === currentNode.value) return undefined;
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+}
---
+++ src/functions/data-structures/binary-search-tree/__test__/BinarySearchTree.test.js
@@ -0,0 +1,13 @@
+import { BinarySearchTree, Node } from "../BinarySearchTree";
+
+const tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+
+test("tree", () => expect(tree.root.left.right.value).toBe(9));
+test("insert", () => {
+  tree.insert(6);
+  expect(tree.root.left.left.value).toBe(6);
+});
