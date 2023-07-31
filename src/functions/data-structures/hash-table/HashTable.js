ea08cda79eabe137b5934c7b5a706f80754d1f51
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-07-31 13:20:02 +0900

    feat: Add HashTable class

--- src/functions/data-structures/hash-table/HashTable.js
+++ src/functions/data-structures/hash-table/HashTable.js
@@ -8,3 +8,19 @@ export const hash = (key, arraylength) => {
   }
   return total;
 };
+
+export class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    let PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
