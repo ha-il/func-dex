63c65c77401d36d9c0b0df7e88b79f823512eacb
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-08-17 11:20:42 +0900

    feat: Add depthFirstIterative method to Graph class

--- src/functions/data-structures/graph/Graph.js
+++ src/functions/data-structures/graph/Graph.js
@@ -42,6 +42,26 @@ class Graph {
 
     return result;
   }
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
   breadthFirst(start) {
     const queue = [start];
     const result = [];
